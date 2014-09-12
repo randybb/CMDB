da0c9e9319494d82643140c98fc4e1133321e978
Roman Priesol <roman@priesol.net>
2014-09-12 22:36:46 +0200

    Model Equipment cleanout

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -12,33 +12,4 @@ class Equipment
   field :timestamps, type: Hash
   field :file_config, type: BSON::Code
   field :device, type: Hash
-
-=begin
-  view :all, key: :name, :conditions => 'doc.cmdb'
-  view :wlc, key: :name, :conditions => 'doc.cmdb.type == "Wireless Controller"'
-  view :test, key: :created_at, :conditions => 'doc.cmdb.type == "Wireless Controller"'
-  view(:for_sitid,
-       :map => %q{
-function(doc){
-  if(doc.ruby_class && doc.ruby_class == 'Equipment') {
-    emit(doc.cmdb.sitid, null);
-  }
-}
-  },
-       :include_docs => true, :type => :custom)
-  view(:find_by_alias,
-       :map => %q{
-function(doc){
-  if(doc.ruby_class && doc.ruby_class == 'Equipment') {
-    if(!doc.cmdb.alias_equipment || doc.cmdb.alias_equipment == "" || doc.cmdb.alias_equipment == "none"){
-      var hostname = doc.name.replace(".lan.skf.net","").toLowerCase();
-    } else {
-      var hostname = doc.cmdb.alias_equipment.toLowerCase();
-    }
-    emit(hostname, null);
-  }
-}
-  },
-       :include_docs => true, :type => :custom)
-=end
 end
